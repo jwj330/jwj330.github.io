dd9aab13fcc907a16d9587e7c72f75c5d79e5fdf
jwj330 <236150597+jwj330@users.noreply.github.com>
2025-10-25 23:19:14 +0800

    Update generate.py

--- script/generate.py
+++ script/generate.py
@@ -2,8 +2,14 @@ import os
 import datetime
 
 def main():
-    # 获取当前时间并格式化
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d")
+    # 获取当前时间
+    current_time = datetime.now()
+
+    # 格式化日期字符串（用于date字段）
+    formatted_date = current_time.strftime('%Y-%m-%d %H:%M:%S+0000')
+
+    # 格式化日期字符串（用于title字段）
+    title_date = current_time.strftime('%Y%m%d')
     
     # 构建目标目录路径
     post_dir = os.path.join("content", "post", f"post_{current_time}")
@@ -14,7 +20,20 @@ def main():
     # 创建并写入index.md文件
     index_path = os.path.join(post_dir, "index.md")
     with open(index_path, "w") as f:
-        f.write("hello")
+        f.write(f"""---
+title: report{title_date}ai
+description: 今日趋势
+slug: report
+date: {formatted_date}
+categories:
+    - 每日Github趋势
+tags:
+    - hugo
+    - Java
+weight: 1       # You can add weight to some posts to override the default sorting (date descending)
+---
+# 今日趋势
+""")
 
 if __name__ == "__main__":
     main()
